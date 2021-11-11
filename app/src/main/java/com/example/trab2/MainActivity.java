b52a90361b04b9103823f7aa1199936b2dfa29b9
TiagoSanti <53698082+TiagoSanti@users.noreply.github.com>
2021-11-10 23:16:45 -0400

    Atividade Main vazia direcionando para a tela de login inicialmente

--- app/src/main/java/com/example/trab2/MainActivity.java
+++ app/src/main/java/com/example/trab2/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.trab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, Login.class));
     }
 }
