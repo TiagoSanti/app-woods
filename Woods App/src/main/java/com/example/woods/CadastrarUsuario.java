3ad1bc0267ed5f9f34df1e3734724070176a9b8f
TiagoSanti <53698082+TiagoSanti@users.noreply.github.com>
2021-11-15 04:41:28 -0400

    Cadastro de usuário implementado

--- Woods App/src/main/java/com/example/woods/CadastrarUsuario.java
+++ Woods App/src/main/java/com/example/woods/CadastrarUsuario.java
@@ -1,27 +1,42 @@
 package com.example.woods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
 public class CadastrarUsuario extends AppCompatActivity {
 
-    EditText edtUsername, edtSenha, edtConfirmarSenha;
-    TextView txtErroCadastro;
-    Button btnCriarConta;
+    private EditText edtEmail, edtSenha, edtConfirmarSenha;
+    private TextView txtErroCadastro;
+    private Button btnCriarConta;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_usuario);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        edtUsername = findViewById(R.id.edtEmailCadastro);
-        edtSenha = findViewById(R.id.edtSenha);
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edtEmailCadastro);
+        edtSenha = findViewById(R.id.edtSenhaCadastro);
         edtConfirmarSenha = findViewById(R.id.edtConfirmarSenha);
         txtErroCadastro = findViewById(R.id.txtErroCadastro);
         btnCriarConta = findViewById(R.id.btnCriarConta);
@@ -31,13 +46,13 @@ public class CadastrarUsuario extends AppCompatActivity {
         btnCriarConta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username, senha, confSenha;
+                String email, senha, confSenha;
 
-                username = edtUsername.getText().toString();
+                email = edtEmail.getText().toString();
                 senha = edtSenha.getText().toString();
                 confSenha = edtConfirmarSenha.getText().toString();
 
-                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(senha) || TextUtils.isEmpty(confSenha)) {
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(senha) || TextUtils.isEmpty(confSenha)) {
                     txtErroCadastro.setText("Existe(m) campo(s) não preenchido(s)");
                     txtErroCadastro.setVisibility(View.VISIBLE);
                 }
@@ -45,11 +60,30 @@ public class CadastrarUsuario extends AppCompatActivity {
                     txtErroCadastro.setText("Senhas não coincidem");
                     txtErroCadastro.setVisibility(View.VISIBLE);
                 }
-                //else if(EMAIL JÁ CADASTRADO) {
-
-                // }
                 else {
-                    //CADASTRAR USUARIO
+
+                    mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                txtErroCadastro.setVisibility(View.INVISIBLE);
+                                startActivity(new Intent(CadastrarUsuario.this, MainActivity.class));
+                                finish();
+                            }
+                            else {
+                                String erro = task.getException().getMessage();
+
+                                if(erro.equals("The email address is already in use by another account.")) {
+                                    txtErroCadastro.setText("O email inserido já está cadastrado.");
+                                    txtErroCadastro.setVisibility(View.VISIBLE);
+                                }
+                                else if(erro.equals("The given password is invalid. [ Password should be at least 6 characters ]")){
+                                    txtErroCadastro.setText("A senha deve conter, pelo menos, 6 caracteres.");
+                                    txtErroCadastro.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        }
+                    });
                 }
             }
         });
